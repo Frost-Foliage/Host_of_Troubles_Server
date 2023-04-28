7111d51d4d91055834ddf30c27d68471a2110942
Frost-Foliage <79428367+Frost-Foliage@users.noreply.github.com>
2023-04-28 19:02:53 +0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,123 +1,95 @@
 import socket
-import re
-import argparse
 import json
 
-# 0: Reject; 1: Prefer first; 2: Prefer last
-multiple_host = 1
-
-# 0: Not recognize; 1: Recognize; 2: Reject
-space_preceded_host_first = 1
-
-# 0: Line folding; 1: Not recognize
-space_preceded_host_other = 0
-
-# 0: Not Recognize; 1: Recognize
-space_succeed_host = 1
-
-# 0: Recognize HTTP, not others
-# 1: Recognize HTTP/S, reject others
-# 2: Recognize HTTP/S, not others
-# 3: Recognize any schema
-schema_of_absolute_URL = 0
-
-# 0: Absolute-URL; 1: Host header
-preference = 0
-
 def request_handler(new_client_socket):
+
+    Consistency = 0
+    Multiple_host_headers = 0
+    Space_preceded_Host_as_first_header = 0
+    Other_space_preceded_host_header = 0
+    Space_succeeded_host_header = 0
 
     request_data = new_client_socket.recv(1024).decode()
     print(request_data)
     request_body = request_data.splitlines(True)[-1]
 
-    # host step0: collect potential headers
     request_line = request_data.splitlines(True)[0]
     request_headers = request_data.splitlines(True)[1:-2]
     first_header = request_headers[0]
     other_headers = request_headers[1:]
 
-    status_code = 200
-    
-    recognized_headers = []
     host_headers = []
-    host_header = ""
-    host_URL = ""
-    host = ""
+    absolute_URI = request_line.split(" ")[1]
 
-    # host step1: recognize headers
-    # host step1-1: first header
-    if first_header[0] == " ":
-        if space_preceded_host_first == 1:
-            recognized_headers.append(first_header[1:])
-        elif space_preceded_host_first == 2:
-            status_code = 400
-    elif first_header[-3] == " ":
-        if space_succeed_host == 1:
-            recognized_headers.append(first_header[:-3] + first_header[-2:])
+    non_match = ""
+    if first_header.startswith("If-None-Match: ") or first_header.startswith("if-none-match: "):
+        non_match = first_header[15:-2]
     else:
-        recognized_headers.append(first_header)
-    # host step1-2: other headers
-    if len(other_headers) > 0:
         for other_header in other_headers:
-            if other_header[0] == " ":
-                if space_preceded_host_other == 0:
-                    recognized_headers[-1] = recognized_headers[-1][:-2] + other_header
-            elif other_header[-3] == " ":
-                if space_succeed_host == 1:
-                    recognized_headers.append(first_header[:-3] + first_header[-2:])
-            else:
-                recognized_headers.append(other_header)    
+            if other_header.startswith("If-None-Match: ") or other_header.startswith("if-none-match: "):
+                non_match = other_header[15:-2]
+                break
+    
+    if first_header.startswith(" "):
+        Space_preceded_Host_as_first_header = 1
+    for other_header in other_headers:
+        if other_header.startswith(" "):
+            Other_space_preceded_host_header = 1
+            break
 
-    # host step2: choose a host header
-    host_headers = [header for header in recognized_headers if header.startswith("Host:")]
+    if first_header.endswith(" \r\n"):
+        Space_succeeded_host_header = 1
+    else:
+        for other_header in other_headers:
+            if other_header.endswith(" \r\n"):
+                Space_succeeded_host_header = 1
+                break
+    
+    if first_header.startswith("host:"):
+        host_headers.append(first_header)
+    for other_header in other_headers:
+        if other_header.startswith(" "):
+            host_headers.append(other_header)
     if len(host_headers) > 1:
-        if multiple_host == 0:
-            status_code = 400
-        elif multiple_host == 1:
-            host_header = host_headers[0]
-        elif multiple_host == 2:
-            host_header = host_header[-1]
-    elif len(host_headers) == 1:
-        host_header = host_headers[0]
-
-    # host step3: get URL host
-    absolute_URL = request_line.split(" ")[1]
-    if absolute_URL != "/":
-        absolute_host = re.sub("(.*): // ", "", absolute_URL)
-    if absolute_URL.startswith("http://"):
-        host_URL = absolute_host
-    elif absolute_URL.startswith("https://"):
-        if schema_of_absolute_URL != 0:
-            host_URL = absolute_host
-    else:
-        if schema_of_absolute_URL == 1:
-            status_code = 400
-        elif schema_of_absolute_URL == 3:
-            host_URL = absolute_host
-
-    # host step3: choose between host header and absolute-URL
-    if host_URL != "" and preference == 0:
-        host = host_URL
-    else:
-        host = host_header    
+        Multiple_host_headers = 1
+    
+    header_host = host_headers[0][6:-2]
+    header_URI = absolute_URI.split("/")[-1]
+    if header_host != header_URI:
+        Consistency = 1
 
     response_line = "HTTP/1.1 200 OK\r\n"
-    if status_code == 400:
-        response_line = "HTTP/1.1 400 Bad Request\r\n"
-    
+
     response_headers = ["server: Apache Tomcat/5.0.12\r\n",
                         "content-type: text/css\r\n",
                         "cache-control: public, max-age=1000\r\n"]
     response_blank = "\r\n"
-    response_body = ""
-    print(request_body)    
-    try:
-        request_dict = json.loads(json.loads(request_body))
-        response_body = request_dict["number"]
-    except:
+    response_body = {"host": host_headers[0][6:-2]}
+
+    if non_match != "" and non_match == host_headers[0][6:-2]:
+        response_line = "HTTP/1.1 304 Not Modified\r\n"
         response_body = ""
-    if status_code == 400:
-        response_body = ""
+    else:
+        if Consistency:
+            response_body = "Ambiguous request: consistency"
+        elif Multiple_host_headers:
+            response_body = "Ambiguous request: multiple host headers"
+        elif Space_preceded_Host_as_first_header:
+            response_body = "Ambiguous request: space-preceded host as first header"
+        elif Other_space_preceded_host_header:
+            response_body = "Ambiguous request: other space-preceded host header"
+        elif Space_succeeded_host_header:
+            response_body = "Ambiguous request: space-succeeded host header"
+        else:
+            try:
+                request_dict = json.loads(json.loads(request_body))
+                response_body["number"] = request_dict["number"]
+                response_body = json.dumps(response_body)
+            except:
+                response_body = json.dumps(response_body)
+        
+        etag_header = "ETag: " + host_headers[0][6:-2] + "\r\n"
+        response_headers.append(etag_header)
 
     response_data = response_line
     for header in response_headers:
@@ -127,27 +99,6 @@ def request_handler(new_client_socket):
 
     new_client_socket.send((response_data.encode()))
     new_client_socket.close()
-
-Parser = argparse.ArgumentParser()
-Parser.add_argument('-m', '--multiple_host', help = '多host头')
-Parser.add_argument('-f', '--space_preceded_host_first', help = '首个头空格前缀')
-Parser.add_argument('-o', '--space_preceded_host_other', help = '其余头空格前缀')
-Parser.add_argument('-s', '--space_succeed_host', help = '头空格后缀')
-Parser.add_argument('-a', '--schema_of_absolute_URL', help = '可接受的绝对URL格式')
-Parser.add_argument('-p', '--preference', help = '同时存在host头和绝对URL时的选择')
-Args = Parser.parse_args()
-if Args.multiple_host:
-    multiple_host = int(Args.multiple_host)
-if Args.space_preceded_host_first:
-    space_preceded_host_first = int(Args.space_preceded_host_first)
-if Args.space_preceded_host_other:
-    space_preceded_host_other = int(Args.space_preceded_host_other)
-if Args.space_succeed_host:
-    space_succeed_host = int(Args.space_succeed_host)
-if Args.schema_of_absolute_URL:
-    schema_of_absolute_URL = int(Args.schema_of_absolute_URL)
-if Args.preference:
-    preference = int(Args.preference)
 
 tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_server_socket.bind(("", 5555))
